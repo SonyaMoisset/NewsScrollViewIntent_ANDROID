e81fdb81c73d522b0dd6d8d5a99b20105fa541a3
smoisset <smoisset@worldremit.com>
2018-01-13 23:10:13 +0000

    Add methods in MainActivity

--- app/src/main/java/com/example/android/newsscrollviewintent/MainActivity.java
+++ app/src/main/java/com/example/android/newsscrollviewintent/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.newsscrollviewintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showFirstNews(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void showSecondNews(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void showThirdNews(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
